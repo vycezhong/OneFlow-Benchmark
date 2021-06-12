dcdab5643b798ff31c72f201e6bbdecb2f88712b
vycezhong <izhongyuchen@gmail.com>
2021-06-12 10:03:22 +0800

    update

--- Classification/cnns/train_fp16.sh
+++ Classification/cnns/train_fp16.sh
@@ -2,17 +2,17 @@ rm -rf core.*
 rm -rf ./output/snapshots/*
 
 if [ -n "$1" ]; then
-    NUM_EPOCH=$1
+	NUM_EPOCH=$1
 else
-    NUM_EPOCH=50
+	NUM_EPOCH=90
 fi
 echo NUM_EPOCH=$NUM_EPOCH
 
 # training with imagenet
 if [ -n "$2" ]; then
-    DATA_ROOT=$2
+	DATA_ROOT=$2
 else
-    DATA_ROOT=/data/imagenet/ofrecord
+	DATA_ROOT=/data/ILSVRC2012/ofrecord
 fi
 echo DATA_ROOT=$DATA_ROOT
 
@@ -26,28 +26,29 @@ export NCCL_LAUNCH_MODE=PARALLEL
 echo NCCL_LAUNCH_MODE=$NCCL_LAUNCH_MODE
 
 python3 of_cnn_train_val.py \
-     --train_data_dir=$DATA_ROOT/train \
-     --train_data_part_num=256 \
-     --val_data_dir=$DATA_ROOT/validation \
-     --val_data_part_num=256 \
-     --num_nodes=1 \
-     --gpu_num_per_node=8 \
-     --optimizer="sgd" \
-     --momentum=0.875 \
-     --label_smoothing=0.1 \
-     --learning_rate=1.536 \
-     --loss_print_every_n_iter=100 \
-     --batch_size_per_device=192 \
-     --val_batch_size_per_device=50 \
-     --use_fp16 \
-     --channel_last=True \
-     --pad_output \
-     --fuse_bn_relu=True \
-     --fuse_bn_add_relu=True \
-     --nccl_fusion_threshold_mb=16 \
-     --nccl_fusion_max_ops=24 \
-     --gpu_image_decoder=True \
-     --num_epoch=$NUM_EPOCH \
-     --model="resnet50" 2>&1 | tee ${LOGFILE}
+	--train_data_dir=$DATA_ROOT/train \
+	--train_data_part_num=256 \
+	--val_data_dir=$DATA_ROOT/validation \
+	--val_data_part_num=256 \
+	--num_nodes=1 \
+	--node_ips="127.0.0.1" \
+	--gpu_num_per_node=4 \
+	--optimizer="sgd" \
+	--momentum=0.875 \
+	--label_smoothing=0.1 \
+	--learning_rate=2.048 \
+	--loss_print_every_n_iter=100 \
+	--batch_size_per_device=512 \
+	--val_batch_size_per_device=100 \
+	--use_fp16 \
+	--channel_last=True \
+	--pad_output \
+	--fuse_bn_relu=True \
+	--fuse_bn_add_relu=True \
+	--nccl_fusion_threshold_mb=4 \
+	--nccl_fusion_max_ops=6 \
+	--gpu_image_decoder=True \
+	--num_epoch=$NUM_EPOCH \
+	--model="resnet50" 2>&1 | tee ${LOGFILE}
 
 echo "Writting log to ${LOGFILE}"
